3da62a5f4488f5f1a4b79b9ebdf58f572b4797bc
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2023-05-26 16:57:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import as3toplevel
+
+setup(name="as3toplevel",
+      version=as3toplevel.__version__,
+      author="ajdelguidice",
+      url="https://github.com/ajdelguidice/python-as3toplevel",
+     py_modules=["as3toplevel"],
+     description="Python implementation of the ActionScript3 toplevel",
+     python_requires=">=3.8",
+     )
