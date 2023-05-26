a39a042a6f5cbb90daa9ebca40aaab28eba136a1
ajdelguidice <67966644+ajdelguidice@users.noreply.github.com>
2023-05-26 18:07:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup
 import as3toplevel
 
+with open("README.md", "r") as ld:
+      long_desc = ld.read()
+
 setup(name="as3toplevel",
       version=as3toplevel.__version__,
       author="ajdelguidice",
-      author_email="ajdelguidice@gmail.com"
+      author_email="ajdelguidice@gmail.com",
       url="https://github.com/ajdelguidice/python-as3toplevel",
       py_modules=["as3toplevel"],
       description="Python implementation of the ActionScript3 toplevel",
+      long_description=long_desc,
       classifiers=[
             "Development Status :: 4 - Beta",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3.8",
-            "Topic :: Utilities"
+            "Topic :: Utilities",
             ],
       python_requires=">=3.8",
-      install_requires=["numpy"],
+      install_requires=["numpy",],
       )
